59d9c0aec2bf3a5401b0040f8ebd64a222025067
Narongkon-arm <52952233+Narongkon-arm@users.noreply.github.com>
2024-02-25 17:31:55 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+require('dotenv').config();
+
+require('./config/database').connect();
+
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+module.exports = app;
