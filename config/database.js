171fa5df29f2bcec4451665eb97af145bf17836c
Narongkon-arm <52952233+Narongkon-arm@users.noreply.github.com>
2024-02-25 17:05:32 +0700

    edit database.js

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+    mongoose.connect(MONGO_URI,{
+
+    })
+    .then(() =>{
+        console.log("Successfully connect to database");
+    })
+    .catch((error) =>{
+        console.log("Error connecting to database");
+        console.error(error);
+        process.exit(1);
+    });
+}
